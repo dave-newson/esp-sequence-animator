5bd58ba7430ac940ad99b9ba7abc5c26ccff35d4
Dave Newson <dave@davenewson.com>
2021-09-14 20:08:52 +1000

    Improved loader to allow sequence changing without writing to device.

--- examples/load.sh
+++ examples/load.sh
@@ -8,23 +8,29 @@ SEQID="$2"
 FILE="$3"
 
 # Guard: IP must be set
-if [[ -z "$3" ]]; then
-    echo "Usage:"
-    echo "load.sh <ip> <sequence_int> <filename.json>"
+if [[ -z "$2" ]]; then
+    echo "Animation Sequence Loader"
+    echo "Play only: ./load.sh <ip> <sequence_int>"
+    echo "Write JSON to device: ./load.sh <ip> <sequence_int> <filename.json>"
     exit 1
 fi
 
-# Read file
-PAYLOAD="$(cat $FILE)"
+# Write to device if file specified
+if [[ $FILE ]]; then
+    echo "Loading $FILE to device ..."
 
-# Load, then play
-curl -s --request POST -H "Content-Type: application/json" http://$IPADDR/api/sequence?id=$SEQID --data "$PAYLOAD"
-echo ""
-echo ""
-curl -s http://$IPADDR/api/sequence?id=$SEQID
-echo ""
-echo ""
+    # Read file
+    PAYLOAD="$(cat $FILE)"
+
+    # Write
+    curl -s --request POST -H "Content-Type: application/json" http://$IPADDR/api/sequence?id=$SEQID --data "$PAYLOAD"
+    echo ""
+    curl -s http://$IPADDR/api/sequence?id=$SEQID
+fi
+
+echo "Starting sequence $SEQID ..."
 curl -s --request POST http://$IPADDR/api/play?id=$SEQID
+echo ""
 echo "Done."
 
 
